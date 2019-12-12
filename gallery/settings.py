7ec481d48c2d8d099034b609612c33c3977d92cf
owinolawrence <owinolawrence21@gmail.com>
2019-12-12 21:43:12 +0300

    define bootstra3 conncts to django-bootstrap dependecy

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'photos.apps.PhotosConfig',
+    'bootstrap3'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
