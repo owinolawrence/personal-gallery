f5fa7291c27fed738062fdb38d8df570187df922
owinolawrence <owinolawrence21@gmail.com>
2019-12-13 16:05:11 +0300

    add admin dashboard url

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     url('^$', views.photos_today, name= 'photosToday'),
     # url('^$',views.index,name = 'index'),
     # url('^today/$', views.photos_of_today, name='photosToday'),
+    url(r'^admin/', views.admin_dashboard,name = "admin_dashboard"),
    url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_photos, name = 'pastPhotos'),
    url(r'^search/', views.search_results, name='search_results')
 ]
