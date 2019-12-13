571d46214b70be34d7ab2d69569436de97761fd7
owinolawrence <owinolawrence21@gmail.com>
2019-12-13 16:10:32 +0300

    add location and images html files

--- photos/admin.py
+++ photos/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Location,Category,Images
+from .models import Location,Category,Image
 
 admin.site.register(Location)
 admin.site.register(Category)
-admin.site.register(Images)
+admin.site.register(Image)
