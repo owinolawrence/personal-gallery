62c17deee0d74dc5c49e39e10f5bc7cbacf53c3e
owinolawrence <owinolawrence21@gmail.com>
2019-12-13 11:32:13 +0300

    create class profile with variables to represent column in database

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models:Model):
+    first_name = models.CharField(max_length =40)
+    last_name = models.CharField(max_length = 40)
+    email=models.EmailField()
+    
