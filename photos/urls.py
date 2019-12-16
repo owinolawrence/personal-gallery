8ceff580e91634eed02de102fc8a501a2ef0be72
owinolawrence <owinolawrence21@gmail.com>
2019-12-16 18:19:14 +0300

    add url link to index.html

--- photos/urls.py
+++ photos/urls.py
@@ -5,8 +5,7 @@ from .import views
 
 
 urlpatterns=[
-    url('^$', views.photos_today, name= 'photosToday'),
-    # url('^$',views.index,name = 'index'),
+   url('^$',views.index,name = 'index'),
     url(r'^category/(\d+)',views.category,name ='category'),
     url(r'^admin/', views.admin_dashboard,name = "admin_dashboard"),
    url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_photos, name = 'pastPhotos'),
