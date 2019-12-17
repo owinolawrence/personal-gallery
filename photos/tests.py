574f37a1e3cd8da443cd413ff2a97300bb0ca2c7
owinolawrence <owinolawrence21@gmail.com>
2019-12-17 07:32:29 +0300

    add styles body tag to change the theme color

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Category, Location,Image
+
+
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Location(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+        #Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Location))
+
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.james.save_location()
+        location = Location.objects.all()
+        self.assertTrue(len(editors) > 0)
+
+    def test_delete_method(self):
+        self.james.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
